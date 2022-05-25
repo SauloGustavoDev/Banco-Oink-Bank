460b19b8ac94140c0cc58c8792996a2d6d4c2b99
lgiurizato <lucas.petros@outlook.com>
2022-05-25 09:16:14 -0300

    Revert "Correção erro Themas"
    This reverts commit 101dde6dfd412d76defbdad5081a0dc04e5e8a59.

--- app/src/main/java/saulodev/com/integrationproject/HomeViewModel.java
+++
@@ -1,7 +0,0 @@
-package saulodev.com.integrationproject;
-
-import androidx.lifecycle.ViewModel;
-
-public class HomeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/saulodev/com/integrationproject/ui/activity/HomeActivity.java
+++ app/src/main/java/saulodev/com/integrationproject/ui/activity/HomeActivity.java
@@ -1,12 +1,10 @@
 package saulodev.com.integrationproject.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 import saulodev.com.integrationproject.R;
-import saulodev.com.integrationproject.ui.fragment.HomeFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -16,12 +14,5 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         //Replace Home Fragment Here
-        replaceFragment();
-    }
-
-    public void replaceFragment(){
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.frame, new HomeFragment()).addToBackStack(null).commit();
     }
 }
