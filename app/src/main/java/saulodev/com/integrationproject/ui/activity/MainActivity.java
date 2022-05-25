a9a8a8fdedae132c50831802bc0a9ca19d501f86
ViniciusSPSilva <vinicius.sspsilva0@gmail.com>
2022-05-25 09:42:40 -0300

    Tela main finalizada

--- app/src/main/java/saulodev/com/integrationproject/ui/activity/MainActivity.java
+++ app/src/main/java/saulodev/com/integrationproject/ui/activity/MainActivity.java
@@ -2,16 +2,47 @@ package saulodev.com.integrationproject.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import saulodev.com.integrationproject.R;
+import saulodev.com.integrationproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+
+        btnRegistrar();
+
+        btnEntrar();
+
+    }
+
+    private void btnEntrar() {
+        binding.btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void btnRegistrar() {
+        binding.btnRegistar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
     }
 }
