52645de70b5bdea79a93181f180ee6122aa5ce63
lgiurizato <lucas.petros@outlook.com>
2022-05-25 15:09:57 -0300

    Ajuste icons Tela Home

--- app/src/main/java/saulodev/com/integrationproject/ui/activity/HomeActivity.java
+++ app/src/main/java/saulodev/com/integrationproject/ui/activity/HomeActivity.java
@@ -1,9 +1,14 @@
 package saulodev.com.integrationproject.ui.activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.WindowDecorActionBar;
 import androidx.fragment.app.FragmentManager;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import saulodev.com.integrationproject.R;
 import saulodev.com.integrationproject.ui.fragment.HomeFragment;
