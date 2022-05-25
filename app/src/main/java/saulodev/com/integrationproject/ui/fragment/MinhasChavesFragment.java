f86f7bab84b80882b4ad80eac5330219b439f175
rod-sousa <rod.sousa899@gmail.com>
2022-05-25 15:54:28 -0300

    resolvido os conflitos

---
+++ app/src/main/java/saulodev/com/integrationproject/ui/fragment/MinhasChavesFragment.java
@@ -0,0 +1,23 @@
+package saulodev.com.integrationproject.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import saulodev.com.integrationproject.R;
+
+public class MinhasChavesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_minhas_chaves, container, false);
+
+    }
+}
