0e2dcdb61f38f9496de04d2b8cf2fa491b0637f7
Lucas Constantino <lucasconstantino0102@gmail.com>
2022-05-25 15:07:40 -0300

    Icones e Area Pix

---
+++ app/src/main/java/saulodev/com/integrationproject/ui/fragment/AreaPixFragment.java
@@ -0,0 +1,22 @@
+package saulodev.com.integrationproject.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import saulodev.com.integrationproject.R;
+
+
+public class AreaPixFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_area_pix, container, false);
+    }
+}
