8c83af7dcef7698e01301e42094f1215de4e1a5f
Henrique Siqueira da Silva <hesiqueira@server01.foursys.local>
2022-05-26 15:36:56 -0300

    configurando requisições API

--- app/src/main/java/saulodev/com/integrationproject/client/Methods.java
+++ app/src/main/java/saulodev/com/integrationproject/client/Methods.java
@@ -1,4 +1,13 @@
 package saulodev.com.integrationproject.client;
 
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import saulodev.com.integrationproject.model.Cliente;
+
 public interface Methods {
+
+    @POST("/clientes")
+    Call<Cliente> cadastrarUsuario(@Body Cliente cliente);
+    
 }
