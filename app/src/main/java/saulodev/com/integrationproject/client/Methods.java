ddb46bbf9ab6854471c7fff6220718b8404fe33a
lgiurizato <lucas.petros@outlook.com>
2022-05-30 11:11:15 -0300

    Retrofit Get Cliente

--- app/src/main/java/saulodev/com/integrationproject/client/Methods.java
+++ app/src/main/java/saulodev/com/integrationproject/client/Methods.java
@@ -1,13 +1,20 @@
 package saulodev.com.integrationproject.client;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 import saulodev.com.integrationproject.model.Cliente;
 
 public interface Methods {
 
     @POST("/clientes")
     Call<Cliente> cadastrarUsuario(@Body Cliente cliente);
+
+    @GET("/clientes/{id}")
+    Call<Cliente> searchUser(@Path("id") Long id);
 
 }
