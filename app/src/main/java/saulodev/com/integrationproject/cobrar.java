2598d50cd1336533fc4ee5f4930268040d237cdf
Pedro Mendes Palharini <ppalharini@server01.foursys.local>
2022-06-07 10:29:03 -0300

    add telas novas

---
+++ app/src/main/java/saulodev/com/integrationproject/cobrar.java
@@ -0,0 +1,13 @@
+package saulodev.com.integrationproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cobrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cobrar);
+    }
+}
---
+++ app/src/main/java/saulodev/com/integrationproject/codigo_de_cobranca.java
@@ -0,0 +1,14 @@
+package saulodev.com.integrationproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class codigo_de_cobranca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_codigo_de_cobranca);
+    }
+}
