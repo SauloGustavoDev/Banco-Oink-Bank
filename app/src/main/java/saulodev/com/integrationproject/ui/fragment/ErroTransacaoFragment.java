3cca1f1fe7b69f9aa8ab0b84ab7bb6d103573dbf
rod-sousa <rod.sousa899@gmail.com>
2022-06-14 11:20:51 -0300

    Realizando a implementação fragment_erro_transacao

--- app/src/main/java/saulodev/com/integrationproject/ui/fragment/ErroTransacaoFragment.java
+++ app/src/main/java/saulodev/com/integrationproject/ui/fragment/ErroTransacaoFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import saulodev.com.integrationproject.databinding.FragmentErroTransacaoBinding;
