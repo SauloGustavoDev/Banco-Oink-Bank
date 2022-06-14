3c60316e00c4af647fd30afaf74ef4fb00df3f56
ViniciusSPSilva <vinicius.sspsilva0@gmail.com>
2022-06-14 10:47:16 -0300

    Implementado a tela de erro inesperado

---
+++ app/src/main/java/saulodev/com/integrationproject/ui/fragment/ErroInesperadoFragment.java
@@ -0,0 +1,30 @@
+package saulodev.com.integrationproject.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import saulodev.com.integrationproject.R;
+
+public class ErroInesperadoFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_erro_inesperado, container, false);
+    }
+}
