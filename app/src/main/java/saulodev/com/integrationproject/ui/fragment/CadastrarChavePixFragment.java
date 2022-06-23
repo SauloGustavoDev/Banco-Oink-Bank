66967122b2d0a6810d8514ce699e47de551324cf
SauloGustavoDev <saulo0100@outlook.com>
2022-06-23 16:16:13 -0300

    Corrigindo bug de fragment transparent

--- app/src/main/java/saulodev/com/integrationproject/ui/fragment/CadastrarChavePixFragment.java
+++ app/src/main/java/saulodev/com/integrationproject/ui/fragment/CadastrarChavePixFragment.java
@@ -1,21 +1,35 @@
 package saulodev.com.integrationproject.ui.fragment;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.ColorRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import saulodev.com.integrationproject.R;
+import saulodev.com.integrationproject.databinding.ActivityRegisterBinding;
 import saulodev.com.integrationproject.databinding.FragmentCadastrarChavePixBinding;
 
 public class CadastrarChavePixFragment extends Fragment {
 
     private FragmentCadastrarChavePixBinding bind;
+
 
     @Nullable
     @Override
@@ -27,6 +41,7 @@ public class CadastrarChavePixFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        bind.cadastrarChaveLayout.setBackgroundColor(getThemeBackgroundColor());
 
         bind.cadastrarBtn.setOnClickListener(view1 -> {
             requireActivity().onBackPressed();
@@ -36,4 +51,15 @@ public class CadastrarChavePixFragment extends Fragment {
             requireActivity().onBackPressed();
         });
     }
+
+
+    public int getThemeBackgroundColor() {
+        TypedArray array = getActivity().getTheme().obtainStyledAttributes(
+                new int[] { android.R.attr.colorBackground });
+        int backgroundColor = array.getColor(0, 0xFF00FF);
+        array.recycle();
+        return backgroundColor;
+    }
+
+
 }
