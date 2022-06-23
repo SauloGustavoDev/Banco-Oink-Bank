117e8a54747b7625a3e4185302ea83d8b83a9cb6
lgiurizato <lucas.petros@outlook.com>
2022-06-23 17:00:16 -0300

    ajustes logica/layout da tela login

--- app/src/main/java/saulodev/com/integrationproject/ui/activity/LoginActivity.java
+++ app/src/main/java/saulodev/com/integrationproject/ui/activity/LoginActivity.java
@@ -15,6 +15,9 @@ import saulodev.com.integrationproject.util.VerificarDados;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+
+
     ActivityLoginBinding binding = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        edtWatchers();
         btnEntrar();
         btnRegister();
 
@@ -34,13 +38,106 @@ public class LoginActivity extends AppCompatActivity {
 
     private void btnEntrar() {
         binding.entrarBtn.setOnClickListener(view -> {
-        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            String conta = binding.contaComDigitoEdt.getText().toString().trim();
+            String agencia = binding.numeroAgenciaEdt.getText().toString().trim();
+            String senha = binding.passwordEdt.getText().toString().trim();
+
+            if (!conta.isEmpty() && !agencia.isEmpty() &&
+                    !senha.isEmpty()) {
+
+                if (!conta.isEmpty()) {
+
+                    if (agencia.length() != 5) {
+                        binding.numeroAgenciaFil.setError("agencia incorreta");
+
+                    } else {
+                        binding.numeroAgenciaFil.setErrorEnabled(false);
+                    }
+
+                    if (conta.length() != 8) {
+                        binding.numeroContaFil.setError("conta invalida");
+
+                    } else {
+                        binding.numeroContaFil.setErrorEnabled(false);
+                    }
+
+                    if (senha.length() != 6) {
+                        binding.numeroSenhaFil.setError("senha_incorreta");
+
+                    } else {
+                        binding.numeroSenhaFil.setErrorEnabled(false);
+                    }
+
+                } else if (agencia.length() == 5 && conta.length() == 8 && senha.length() == 6) {
+                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    finish();
+
+                }
+
+            }else {
+
+                    if (conta.isEmpty()) {
+                        binding.numeroContaFil.setError("Digite o numero da sua conta");
+
+                    } else {
+                        binding.numeroContaFil.setErrorEnabled(false);
+                    }
+
+                    if (agencia.isEmpty()) {
+                        binding.numeroAgenciaFil.setError("Digite o numero da sua agencia");
+
+                    } else {
+                        binding.numeroAgenciaFil.setErrorEnabled(false);
+                    }
+
+                    if (senha.isEmpty()) {
+                        binding.numeroSenhaFil.setError("Digite sua senha");
+
+                    } else {
+                        binding.numeroSenhaFil.setErrorEnabled(false);
+                    }
+
+                }
         finish();
         });
-    }
+
+        }
+
     private void btnRegister(){
         binding.cadastrarTxt.setOnClickListener(View -> {
             startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
         });
+    }
+
+    private void edtWatchers() {
+
+        binding.numeroAgenciaFil.setOnFocusChangeListener((view, hasFocus) ->{
+
+            if (!hasFocus && binding.numeroAgenciaEdt.getText().toString().trim().isEmpty())
+                binding.numeroAgenciaFil.setError(getString(R.string.campo_obrigatorio));
+            else
+                binding.numeroAgenciaFil.setErrorEnabled(false);
+
+        });
+
+        binding.numeroContaFil.setOnFocusChangeListener((view, hasFocus) ->{
+
+            if (!hasFocus && binding.contaComDigitoEdt.getText().toString().trim().isEmpty())
+                binding.numeroContaFil.setError(getString(R.string.campo_obrigatorio));
+            else
+                binding.numeroContaFil.setErrorEnabled(false);
+
+        });
+
+        binding.numeroSenhaFil.setOnFocusChangeListener((view, hasFocus) ->{
+
+            if (!hasFocus && binding.passwordEdt.getText().toString().trim().isEmpty())
+                binding.numeroSenhaFil.setError(getString(R.string.campo_obrigatorio));
+            else
+                binding.numeroSenhaFil.setErrorEnabled(false);
+
+        });
+
+
     }
 }
