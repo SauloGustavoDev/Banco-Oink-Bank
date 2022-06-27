5a385ae752dffb2e5939a645a583d0729925dc15
lgiurizato <lucas.petros@outlook.com>
2022-06-27 11:33:31 -0300

    add telas de erro pelo alertDialog

--- app/src/main/java/saulodev/com/integrationproject/ui/fragment/AreaPixFragment.java
+++ app/src/main/java/saulodev/com/integrationproject/ui/fragment/AreaPixFragment.java
@@ -1,5 +1,6 @@
 package saulodev.com.integrationproject.ui.fragment;
 
+import static android.R.attr.backgroundStacked;
 import static android.R.attr.colorBackground;
 
 import android.annotation.SuppressLint;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -17,6 +19,7 @@ import android.view.ViewGroup;
 
 import saulodev.com.integrationproject.R;
 import saulodev.com.integrationproject.databinding.FragmentAreaPixBinding;
+import saulodev.com.integrationproject.util.MyAlertDialog;
 
 
 public class AreaPixFragment extends Fragment {
@@ -27,7 +30,8 @@ public class AreaPixFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         bind = FragmentAreaPixBinding.inflate(inflater, container, false);
-        return bind.getRoot();
+        View view = bind.getRoot();
+        return view;
     }
 
     @SuppressLint({"ResourceType", "UseCompatLoadingForDrawables"})
@@ -35,6 +39,27 @@ public class AreaPixFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+        bind.pixQrcodeLinearBtn.setOnClickListener(view1 ->{
+            MyAlertDialog myAlertDialog = new MyAlertDialog(R.layout.fragment_erro_inesperado);
+        DialogFragment dialog = myAlertDialog;
+        dialog.show(requireActivity().getSupportFragmentManager(),"fragment");
+
+        });
+
+        bind.linearPixCelular.setOnClickListener(view1 ->{
+            MyAlertDialog myAlertDialog = new MyAlertDialog(R.layout.fragment_erro_404);
+            DialogFragment dialog = myAlertDialog;
+            dialog.show(requireActivity().getSupportFragmentManager(),"fragment");
+
+        });
+
+        bind.pixCpfLinearBtn.setOnClickListener(view1 ->{
+            MyAlertDialog myAlertDialog = new MyAlertDialog(R.layout.fragment_erro_transacao);
+            DialogFragment dialog = myAlertDialog;
+            dialog.show(requireActivity().getSupportFragmentManager(),"fragment");
+
+        });
 
         bind.minhasChavesBtn.setOnClickListener(view1 -> {
             replaceMinhasChavesFragment();
