31d7441544c7a9d4c2f945d55898cdfff1aba932
Henrique Siqueira da Silva <hesiqueira@server01.foursys.local>
2022-06-27 09:23:18 -0300

    Aplicando Activity CadastroConcluido

--- app/src/main/java/saulodev/com/integrationproject/ui/activity/RegisterActivity.java
+++ app/src/main/java/saulodev/com/integrationproject/ui/activity/RegisterActivity.java
@@ -96,7 +96,7 @@ public class RegisterActivity extends AppCompatActivity {
                     } else {
                         //TODO
 
-                        Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                        Intent intent = new Intent(RegisterActivity.this, CadastroConcluidoActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent);
                         finish();
